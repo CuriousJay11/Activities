fa08a1dec339ba17f7a28de784d78790a188f92f
CuriousJay11 <vijay.gopinathan011@gmail.com>
2025-05-03 11:52:16 +0530

    3/5/25

---
+++ Day 1 Activity 1.py
@@ -0,0 +1,4 @@
+print("Can u come outside to play")
+print("s")
+Z=0
+print("The net worth of elon musk is",Z)
--- Python1.py
+++ Python1.py
@@ -1,3 +1,16 @@
 a = False
 print(a)
 print("type of a",type(a))
+
+b = 1
+print(b)
+print("type of a",type(b))
+
+c = 1.34
+print(c)
+print("type of a",type(c))
+
+d = Hi
+print(d)
+print("type of a",type(d))
+
