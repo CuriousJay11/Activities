3bf84fc222b971865080254ab515a218425168c7
CuriousJay11 <vijay.gopinathan011@gmail.com>
2025-05-04 11:34:48 +0530

    4/5/25

---
+++ Day 1 AfterClass Activity.py
@@ -0,0 +1,21 @@
+friend1 = input("Enter name of friend 1- ")
+bday1 = input("Enter their birthday: ")
+
+friend2 = input("Enter name of friend 2- ")
+bday2 = input("Enter their birthday: ")
+
+friend3 = input("Enter name of friend 3- ")
+bday3 = input("Enter their birthday: ")
+
+friend4 = input("Enter name of friend 4- ")
+bday4 = input("Enter their birthday: ")
+
+friend5 = input("Enter name of friend 5- ")
+bday5 = input("Enter their birthday: ")
+
+# Print birthdays
+print(friend1, "-", bday1)
+print(friend2, "-", bday2)
+print(friend3, "-", bday3)
+print(friend4, "-", bday4)
+print(friend5, "-", bday5)
